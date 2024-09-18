a56f89c7d884f6785a566ea781241f571e569d24
Odokaa (オドカー) <xingdaiyamondo@gmail.com>
2024-09-18 17:49:30 +0800

    2D array

--- test/for/test.js
+++ test/for/test.js
@@ -23,8 +23,78 @@ const prompt = require('prompt-sync')();
 //     arr[i] = prompt('enter value');
 // }
 // for (var i = 0; i < N; i++) {
-//     if(arr[i] > 9999 && arr[i] < 100000)
+//     if(arr[i] > 9999 && arr[i] < 100000){
 //         count++
+//     }
 // }
 // console.log(count);
 
+
+// var arr = [];
+// var N = prompt('garaas n too oruulna uu: ');
+// var K = prompt('garaas K too oruulna uu: ');
+
+// var count = 0;
+// for (var i = 0; i < N; i++) {
+//         arr[i] = prompt('too');
+// }
+// for (var i = 0; i < N; i++) {
+//     if(arr[i] % K ==0){
+//         count++
+//     }
+// }
+// console.log(count);
+
+
+
+
+// var counts = new Array(3); 
+// var tegsh = 0;
+// for (var i = 0; i < 3; i++) {
+//     counts[i] = new Array(3); 
+//     for (var j = 0; j < 3; j++) {
+//     counts[i][j] = Math.floor(Math.random() * 10); 
+
+//     }
+//     console.log(counts); 
+// }
+
+// for (var g = 0; g <= counts.length; g++){
+//     if(counts[g] % 2 == 0){
+//         tegsh++
+//     }
+// }
+// console.log(tegsh)
+
+
+
+let rows = prompt('enter value rows: ');
+let cols = prompt('enter value cols: ');
+
+let arr2D = new Array(rows);
+
+for (let i = 0; i < rows; i++) {
+    arr2D[i] = new Array(cols);
+    for (let j = 0; j < cols; j++) {
+        arr2D[i][j] = Math.floor(Math.random() * 10); 
+    }
+}
+
+
+console.log("2D Array with random numbers:");
+for (let i = 0; i < rows; i++) {
+    console.log(arr2D[i]);
+}
+
+
+let niilber = 0;
+
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        if (arr2D[i][j] % 2 === 0) {
+            niilber += arr2D[i][j];
+        }
+    }
+}
+
+console.log("Tegsh too niilber:", niilber);
