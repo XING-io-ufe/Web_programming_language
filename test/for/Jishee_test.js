4fb38f39d2f4f75346af8862a75f2c2ea7179bee
Odokaa (オドカー) <xingdaiyamondo@gmail.com>
2024-09-18 15:34:51 +0800

    for loop useing test code

---
+++ test/for/Jishee_test.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+var n = prompt('utga: ');
+
+for(var i = 1; i <=n; i++){
+    console.log(i);
+}
---
+++ test/for/test.js
@@ -0,0 +1,30 @@
+const prompt = require('prompt-sync')();
+
+
+// var counts = [ [ 0, 1, 2], [ 3, 3, 5], [ 6, 7, 8] ];
+
+// var counts = [];
+// var c = 0;
+// for (var i = 0; i <3; i++) {
+//     counts[i] = [];
+//     for (var j = 0; j < 3; j++) {
+//         counts[i][j] = c;
+//         console.log(counts)
+//         c++;
+//     }
+// }
+
+
+
+// var arr = [];
+// var N = 5;
+// var count = 0;
+// for (var i = 0; i < N; i++) {
+//     arr[i] = prompt('enter value');
+// }
+// for (var i = 0; i < N; i++) {
+//     if(arr[i] > 9999 && arr[i] < 100000)
+//         count++
+// }
+// console.log(count);
+
